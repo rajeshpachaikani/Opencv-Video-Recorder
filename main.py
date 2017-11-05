9fba2823ebcc96ccdef456d624dd7973ee0a9d5d
Rajesh <rajeshpachaikani@live.in>
2017-11-05 19:31:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-#
-#
-#
-
-
-
+#! python3
+__author__ = "Rajesh Pachaikani"
+__copyright__ = "None"
+__credits__ = ["Opencv Developers"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Author"
+__email__ = "rajeshpachaikani@gmail.com"
+__status__ = "Release"
 
 
 import cv2
@@ -14,24 +17,29 @@ fourcc = cv2.VideoWriter_fourcc(*'MJPG')
 rec = cv2.VideoWriter("output.avi", fourcc,17 , (640, 480))
 flag = False
 font = cv2.FONT_HERSHEY_SIMPLEX
+
 while 1:
     b, img = cam.read()
     if b:
-        cv2.putText(img,"c-Capture  s-Save  q- Quit",(100,50),cv2.FONT_HERSHEY_PLAIN,fontScale=2,color=(100,100,100))
+        cv2.putText(img,"c-Capture  s-Save  q- Quit",(100,50),cv2.FONT_HERSHEY_PLAIN,
+                    fontScale=2,color=(100,100,100), thickness=3)
 
-    key1 = cv2.waitKey(1) & 0XFF
-    if key1 == ord('c'):
+    key = cv2.waitKey(1) & 0XFF
+    if key == ord('c'):
         flag = True
+
     if flag:
         cv2.putText(img, "Recording", (50, 100), cv2.FONT_HERSHEY_PLAIN, fontScale=2,
                     color=(100, 100, 100))
         rec.write(img)
-    if key1 == ord('s'):
+
+    if key == ord('s'):
         flag = False
+
     if not flag:
         rec.release()
 
-    if key1 == ord('q'):
+    if key == ord('q'):
         rec.release()
         break
     cv2.imshow("ViewPort", img)
